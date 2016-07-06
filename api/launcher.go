2dd69f71bbb9a98f8d9c6782a065ad169ff05804
Adrien Domoison <adomoison@gmail.com>
2016-07-06 16:31:56 -0700

    using of the port attributed by heroku

--- api/launcher.go
+++ api/launcher.go
@@ -4,15 +4,26 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
+	"os"
+	"log"
 )
 
 var app *gin.Engine
 
 func InitApi() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Println("Local deployement, use port 5000")
+		port = 5000
+	} else {
+		log.Println("Heroku deployement, use port " + strconv.Itoa(port))
+	}
+
 	app = gin.Default()
 	app.GET("/api/user/:id", func (c *gin.Context) {
 		id, _ := strconv.Atoi(c.Params.ByName("id"))
 		c.JSON(http.StatusOK, gin.H{"id": id, "working": true})
 	})
-	app.Run(":5000")
+	app.Run(":" + port)
 }
