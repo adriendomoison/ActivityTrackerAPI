1e41226686c077ddfc668f4c428c8bdbe0876d25
Adrien Domoison <adomoison@gmail.com>
2016-07-06 16:22:14 -0700

    test of gin on heroku

--- api/launcher.go
+++ api/launcher.go
@@ -2,11 +2,17 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
 var app *gin.Engine
 
 func InitApi() {
 	app = gin.Default()
+	app.GET("/api/user/:id", func (c *gin.Context) {
+		id, _ := strconv.Atoi(c.Params.ByName("id"))
+		c.JSON(http.StatusOK, gin.H{"id": id, "working": true})
+	})
 	app.Run(":8100")
 }
